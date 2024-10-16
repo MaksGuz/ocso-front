8bce80882300268425a6891fe292bd8f4f20b48e
MaksGuz <maax.guzmaan@gmail.com>
2024-10-15 21:42:39 -0600

    test auth

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,21 +1,30 @@
+import { API_URL } from "@/constants";
 import { Button, Input, Link } from "@nextui-org/react"
+import axios from "axios";
 
 
 export default function LoginPage() {
+    const handleSumbit = async (e : React.FormEvent) => {
+        e.preventDefault()
+        const formData = new FormData(e.target);
+        const { data } = await axios.post(`${API_URL}/auth/login`, formData)
+        console.log(data);
+        return;
+    }
     return (
-        <div className="bg-white-10 px-10 py-2 rounded-md">
-        <p className="text-2x1 my-4 text-center">Iniciar sesión</p>
-        <div className="flex flex-col gap-2 my-4 items-center">
-            <Input label= "Email" type="email" isRequired={true} size="sm" />
-            <Input label= "Contraseña" type="password" isRequired={true} size="sm" />
-        </div>
-            <div className="flex flex-col items-center gap-2">
+        <form className="bg-white-10 px-10 py-2 rounded-md">
+            <p className="text-2x1 my-4 text-center">Iniciar sesión</p>
+            <div className="flex flex-col gap-2 my-4 items-center">
+                <Input label= "Email" name="userEmail" type="email" isRequired={true} size="sm" />
+                <Input label= "Contraseña" name="userPassword" type="password" isRequired={true} size="sm" />
+            </div>
+                <div className="flex flex-col items-center gap-2">
 
-         <Button color="primary">Iniciar sesiónn</Button>
-         <p>
-            ¿No tienes una cuenta? <Link href="/signup">Registrarse</Link>
-         </p>
-         </div>
-       </div>
+            <Button color="primary">Iniciar sesión</Button>
+            <p>
+                ¿No tienes una cuenta? <Link href="/signup">Registrarse</Link>
+            </p>
+            </div>
+       </form>
     )
 }
---
+++ app/dashboard/_components/Header.tsx
@@ -0,0 +1,8 @@
+import Image from "next/image"
+export default function Header(){
+    return (
+        <div className="w-screen h-[10v] bg-orange-200 flex flex-row items-center px-10">
+            <Image src="oxxo-logo.svg" width={100} height={0} alt="Ocso Logo" draggable={false}/>
+        </div>
+    )
+}
