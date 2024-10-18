0b6e4da64dba76ac5ac35acc21520f8a9a9f515a
MaksGuz <maax.guzmaan@gmail.com>
2024-10-17 23:12:12 -0600

    feat: Cookies in SSC

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -2,39 +2,49 @@
 import { Button, Input, Link } from "@nextui-org/react"
 import axios from "axios";
 import { API_URL } from "@/constants";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
-    const handleSubmit = async (e : React.FormEvent) => {
+    const [submitting, setSubmitting] = useState(false)
+    const router = useRouter()
+    const handleSubmit = async (e: React.FormEvent) => {
+        setSubmitting(true);
         e.preventDefault()
         const formData = new FormData(e.target);
         let authData: any = {}
         authData.userEmail = formData.get("userEmail");
         authData.userPassword = formData.get("userPassword");
-        const { data } = await axios.post(`${API_URL}/auth/login`, {
-            ...authData
-        },{
-         withCredentials: true,
-        });
-        console.log(data);
+        try {
+            const response = await axios.post(`${API_URL}/auth/login`, {
+                ...authData
+            }, {
+                withCredentials: true,
+            });
+            if (response.status == 201) router.push('/dashboard')
+            setSubmitting(false)
+        } catch (e) {
+            setSubmitting(false)
+        }
         return;
     }
-
     return (
         <form className="bg-white-10 px-10 py-2 rounded-md" onSubmit={handleSubmit}>
             <p className="text-2xl my-4 text-center">
                 Iniciar sesión
-                </p>
+            </p>
             <div className="flex flex-col gap-2 my-4 items-center">
-                <Input label= "Email" name="userEmail" type="email" isRequired={true} size="sm" />
-                <Input label= "Contraseña" name="userPassword" type="password" isRequired={true} size="sm" />
+                <Input label="Email" name="userEmail" type="email" isRequired={true} size="sm" />
+                <Input label="Contraseña" name="userPassword" type="password" isRequired={true} size="sm" />
             </div>
-                <div className="flex flex-col items-center gap-2">
-
-            <Button color="primary" type="submit"> Iniciar sesión</Button>
+            <div className="flex flex-col items-center gap-2">
+                <Button color="primary" type="submit" disabled={submitting}>
+                    {submitting ? "Enviando..." : "Iniciar Sesión"}
+                </Button>
                 <p>
                     ¿No tienes una cuenta? <Link href="/signup">Registrarse</Link>
                 </p>
             </div>
-       </form>
+        </form>
     )
 }
