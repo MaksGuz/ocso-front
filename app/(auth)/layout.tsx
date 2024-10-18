1f1a8606c869a018850d024dcff2bfe505fc6060
MaksGuz <maax.guzmaan@gmail.com>
2024-10-17 20:05:21 -0600

    feat: JWT reatrived

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -6,7 +6,7 @@ export default function AuthLayout ({
 }>) {
     return (
         <div className="bg-white-10 w-screen h-screen overlow-hidden grid">
-         <div className="place-content-center place-self-center" place-items-center text-center> 
+         <div className="place-content-center place-self-center place-items-center text-center"> 
           <div className="flex flex-col items-center bottom-10 relative">
           <Image 
               src="/oxxo-logo.svg"
--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,29 +1,35 @@
-import { API_URL } from "@/constants";
+'use client';
 import { Button, Input, Link } from "@nextui-org/react"
 import axios from "axios";
-
+import { API_URL } from "@/constants";
 
 export default function LoginPage() {
-    const handleSumbit = async (e : React.FormEvent) => {
+    const handleSubmit = async (e : React.FormEvent) => {
         e.preventDefault()
         const formData = new FormData(e.target);
-        const { data } = await axios.post(`${API_URL}/auth/login`, formData)
+        let authData: any = {}
+        authData.userEmail = formData.get("userEmail");
+        authData.userPassword = formData.get("userPassword");
+        const { data } = await axios.post(`${API_URL}/auth/login`, authData)
         console.log(data);
         return;
     }
+
     return (
-        <form className="bg-white-10 px-10 py-2 rounded-md">
-            <p className="text-2x1 my-4 text-center">Iniciar sesión</p>
+        <form className="bg-white-10 px-10 py-2 rounded-md" onSubmit={handleSubmit}>
+            <p className="text-2xl my-4 text-center">
+                Iniciar sesión
+                </p>
             <div className="flex flex-col gap-2 my-4 items-center">
                 <Input label= "Email" name="userEmail" type="email" isRequired={true} size="sm" />
                 <Input label= "Contraseña" name="userPassword" type="password" isRequired={true} size="sm" />
             </div>
                 <div className="flex flex-col items-center gap-2">
 
-            <Button color="primary">Iniciar sesión</Button>
-            <p>
-                ¿No tienes una cuenta? <Link href="/signup">Registrarse</Link>
-            </p>
+            <Button color="primary" type="submit"> Iniciar sesión</Button>
+                <p>
+                    ¿No tienes una cuenta? <Link href="/signup">Registrarse</Link>
+                </p>
             </div>
        </form>
     )
