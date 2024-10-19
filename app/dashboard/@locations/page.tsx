c2397d1be051695928ce0d981dc5d8eea764252f
MaksGuz <maax.guzmaan@gmail.com>
2024-10-19 16:11:05 -0600

    Create post  Ocso-Store

--- app/dashboard/@locations/page.tsx
+++ app/dashboard/@locations/page.tsx
@@ -1,16 +1,17 @@
 import axios from "axios";;
 import { cookies } from "next/headers";
-import { TOKEN_NAME } from "@/constants";
+import { API_URL, TOKEN_NAME } from "@/constants";
 import { Location } from "@/entities";
 import SelectLocation from "./_components/SelectLocation";
 import LocationCard from "./_components/LocationCard";
+import FormNewLocation from "./_components/FormNewLocation";
 const LocationPage = async ({ searchParams }: {
     searchParams: { [key: string]: string | string[] | undefined };
 }) => {
     const userCookies = cookies()
     const token = userCookies.get(TOKEN_NAME)?.value;
     let { data } = await axios.get<Location[]>(
-        "http://127.0.0.1:4000/locations",
+        `${API_URL}/locations`,
         {
             headers: {
                 Authorization: `Bearer ${token}`,
@@ -35,6 +36,7 @@ const LocationPage = async ({ searchParams }: {
                 <div className="8/12">
                     <LocationCard store={searchParams.store}/>
                 </div>
+                <FormNewLocation/>
             </div>
         </div>
     );
