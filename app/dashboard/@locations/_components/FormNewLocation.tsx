f0944b39891e27b748556fedb32004b1cb79ba41
MaksGuz <maax.guzmaan@gmail.com>
2024-10-21 21:17:13 -0600

    feat: caching

--- app/dashboard/@locations/_components/FormNewLocation.tsx
+++ app/dashboard/@locations/_components/FormNewLocation.tsx
@@ -1,10 +1,11 @@
-import { Input } from "@nextui-org/react"
+import { Button, Input } from "@nextui-org/react"
 import { createLocation } from "@/actions/locations/create";
 import axios from "axios";
 import { API_URL, TOKEN_NAME } from "@/constants";
 import { cookies } from "next/headers";
 import SelectManager from "./SelectManager";
-export  default async function FormNewLocation() {
+export  default async function FormNewLocation({store}: {store : string | string[] | undefined}) {
+    if (store) return null
     const token = cookies().get(TOKEN_NAME)?.value;
     const responseManager = await axios.get(`${API_URL}/managers`,{
         headers: {
@@ -17,13 +18,14 @@ export  default async function FormNewLocation() {
         }
     })
     return (
-        <form action={createLocation}>
+        <form action={createLocation} className="bg-orange-400 py-2 px-4 flex flex-col gap-6 w-full rounded-lg">
+            <h1 className="text-3x1 text-white text-center"> Crear tienda </h1>
             <Input label="Nombre" placeholder="Ocso Jurikiya" name="locationName" />
             <Input label="Direccion" placeholder="Av de la Luz" name="locationAddress" />
             <Input label="Latitud"  placeholder="-120" name="locationLat" />
             <Input label="Longitud" placeholder="20" name="locationLng" />
             <SelectManager managers={responseManager.data} locations={responseLocation.data}/>
-            <button type="submit"> Subir </button>
+            <Button type="submit" color="primary"> Subir </Button>
         </form>
     );
 }
--- app/dashboard/@locations/_components/LocationCard.tsx
+++ app/dashboard/@locations/_components/LocationCard.tsx
@@ -1,22 +1,25 @@
-import { API_URL, TOKEN_NAME } from "@/constants";
+import { API_URL } from "@/constants";
 import { Location } from "@/entities";
+import { authHeaders } from "@/helpers/authHeaders";
 import { Card, CardHeader, CardBody, Divider } from "@nextui-org/react";
 import axios from "axios";
-import { cookies } from "next/headers";
 import Link from "next/link";
 
-export default async function LocationCard({ store }: { store: string | string[] | undefined }) {
+export default async function LocationCard({
+     store,
+    }: {
+     store: string | string[] | undefined;
+    }) {
     if (!store) return null;
-    const token = cookies().get(TOKEN_NAME)?.value;
     const { data } = await axios.get<Location>(`${API_URL}/locations/${store}`, {
         headers: {
-            Authorization: `Bearer ${token}`
-        }
-    })
+            ...authHeaders()
+        },
+    });
     return (
         <Card>
             <CardHeader>
-                <b className="w-full">{data.locationName}</b>
+                <b className="w-full text-2x1">{data.locationName}</b>
             </CardHeader>
             <Divider />
             <CardBody>
