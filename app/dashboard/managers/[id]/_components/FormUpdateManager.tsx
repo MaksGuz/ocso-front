099f2480dbef32ca724ebf0b34a46cba097963d4
MaksGuz <maax.guzmaan@gmail.com>
2024-10-27 21:17:55 -0600

    implemented modal for updating

--- app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
+++ app/dashboard/managers/[id]/_components/FormUpdateManager.tsx
@@ -2,7 +2,7 @@ import updateManager from "@/actions/managers/update";
 import { API_URL } from "@/constants";
 import { Manager } from "@/entities";
 import { authHeaders } from "@/helpers/authHeaders";
-import { Button, Input } from "@nextui-org/react";
+import { Button, Input} from "@nextui-org/react";
 import SelectStore from "./SelectStore";
 export default async function FormUpdateManager({manager} : {manager: Manager}) {
     const updateManagerWithId = updateManager.bind(null, manager.managerId)
@@ -13,29 +13,43 @@ export default async function FormUpdateManager({manager} : {manager: Manager}) 
     })
     const stores = await responseStores.json()
     return (
-        <form action={updateManagerWithId} className="bg-orange-400 rounded-md">
+        <form action={updateManagerWithId} className="bg-orange-400 rounded-md flex flex-col flex-grow-0 gap-2">
             <h1> Actualizar Manager </h1>
             <Input 
+            required={true}
+            isRequired
+            label="Nombre completo" 
             defaultValue={manager.managerFullName}
             placeholder="Marco Aurelio"
             name="managerFullName"
             />
             <Input 
+            required={true}
+            isRequired
+            label="Email" 
             defaultValue={manager.managerEmail}
             placeholder="manager@ocso.com"
             name="managerEmail"
             />
             <Input 
+            required={true}
+            isRequired
+            label="Salario" 
             defaultValue={String(manager.managerSalary)}
-            placeholder="$00.000"
-            name="12000"
+            placeholder="$12000"
+            name="managerSalary"
             />
             <Input 
+            required={true}
+            isRequired
+            label="Numero de telefono" 
             defaultValue={manager.managerPhoneNumber}
-            placeholder="Telefono"
-            name="44227348593"
+            placeholder="44227348593"
+            name="managerPhoneNumber"
             />
-            <SelectStore stores={stores} defaultStore={manager?.location?.locationId} />
+            <SelectStore stores={stores} 
+            defaultStore={manager?.location?.locationId} 
+            />
             <Button color="primary" type="submit">
             Actualizar 
             </Button>
