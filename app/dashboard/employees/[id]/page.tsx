e08b60edb18b6075c64a7f4b8887f824439286ea
MaksGuz <maax.guzmaan@gmail.com>
2024-11-17 19:00:21 -0600

    update employees

---
+++ app/dashboard/employees/[id]/page.tsx
@@ -0,0 +1,24 @@
+import { API_URL } from "@/constants";
+import { authHeaders } from "@/helpers/authHeaders";
+import { Employee } from "@/entities";
+import { Image } from "@nextui-org/react";
+import FormUpdateEmployee from "./_components/FormUpdateEmployee";
+import EmployeeDataCard from "./_components/EmployeeDataCarda";
+export default async function EmployeePage({
+  params,
+}: {
+  params: { id: string };
+}) {
+  const responseEmployee = await fetch(`${API_URL}/employees/${params.id}`, {
+    headers: {
+      ...authHeaders(),
+    },
+  });
+  const employee: Employee = await responseEmployee.json();
+  return (
+    <div className="w-full h-[90vh] flex flex-row items-center justify-center">
+      <EmployeeDataCard employee={employee} />
+      <FormUpdateEmployee employee={employee} />
+    </div>
+  );
+}
